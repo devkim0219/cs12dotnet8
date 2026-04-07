6cd733f2a0d2a52eaa0bcc950bae84d8b1d80b3b
devkim0219@gmail.com <devkim0219@gmail.com>
2026-04-07 14:07:40 +0900

    Add : Chapter02 > AsyncConsole project

--- Chapter02/AsyncConsole/Program.cs
+++ Chapter02/AsyncConsole/Program.cs
@@ -1,1 +1,4 @@
-﻿
+﻿HttpClient client = new();
+HttpResponseMessage response = await client.GetAsync("http://www.google.com/");
+
+WriteLine("Google's home page has {0:N0} bytes.", response.Content.Headers.ContentLength);
