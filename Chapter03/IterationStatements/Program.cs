c4f2f3917907cb1157ee4878019f496ebb531ed1
devkim0219@gmail.com <devkim0219@gmail.com>
2026-04-08 16:31:58 +0900

    Update: Chapter03 > IterationStatements project

--- Chapter03/IterationStatements/Program.cs
+++ Chapter03/IterationStatements/Program.cs
@@ -4,3 +4,22 @@ while (x < 10)
     WriteLine(x);
     x++;
 }
+
+string? actualPassword = "Pa$$w0rd";
+string? password;
+
+do
+{
+    WriteLine("Enter your password: ");
+    password = ReadLine();
+}
+while (password != actualPassword);
+
+WriteLine("Correct!");
+
+string[] names = { "Adam", "Barry", "Charlie" };
+
+foreach (string name in names)
+{
+    WriteLine($"{name} has {name.Length} characters.");
+}
